4f17d7b2c72b5851204533e32c38cdbe76b9fd1e
Daniel Gardner <daniel.marc.gardner@gmail.com>
2019-01-08 22:54:04 -0800

    Build five day page.

--- src/components/five-day-card/index.js
+++ src/components/five-day-card/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import moment from 'moment';
+import { func, object, number, string } from 'prop-types';
+import './five-day-card.css';
 
 export const FiveDayCard = (props) => {
-  console.log(props);
-  const { date, day } = props;
+  const { date, day, showMore, index } = props;
   return (
     <div className="fivedaycard">
-      <h4 className="fivedaycard__date">{moment(date).format('dddd, MMMM Do YYYY')}</h4>
-      <p className="fivedaycard__max">{ day.maxtemp_f} &#8457;</p>
-      <p className="fivedaycard__min">{ day.mintemp_f} &#8457;</p>
+      <h4 className="fivedaycard__day">{moment(date).format('dddd')}</h4>
+      <h5 className="fivedaycard__day">{moment(date).format('MMMM Do YYYY')}</h5>
+      <div className="fivedaycard__day--current">
+        <img src={ `https:${day.condition.icon}` } alt={ day.condition.text } className="fivedaycard__day--current-img" />
+      </div>
+      <div className="fivedaycard__temps">
+        <p className="fivedaycard__temps--max">{ day.maxtemp_f} &#8457;</p>
+        <p className="fivedaycard__temps--min">{ day.mintemp_f} &#8457;</p>
+        <span className="fivedaycard__temps--viewmore typ--link" onClick={ () => showMore(index) }>View full report</span>
+      </div>
     </div>
   );
 };
+
+FiveDayCard.propTypes = {
+  day: object,
+  date: string,
+  showMore: func,
+  index: number
+};
+
+FiveDayCard.defaultProps = {
+  showMore: () => {}
+};
+
 export default FiveDayCard;
--- src/routes/fiveday/index.js
+++ src/routes/fiveday/index.js
@@ -1,16 +1,71 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { func, object } from 'prop-types';
+import moment from 'moment';
+import { getWeatherByZipCode, removeZip } from '../../redux/actions/weatherActions';
 import { find } from '../../utils/find';
 import FiveDayCard from '../../components/five-day-card';
+import HeroWeatherDisplay from '../../components/hero-weather-display';
+import './fiveday.css';
 
 export class FiveDay extends Component {
+  constructor(props) {
+    super(props);
+    this.closeAndRemoveZip = this.closeAndRemoveZip.bind(this);
+    this.openSpecificDay = this.openSpecificDay.bind(this);
+    this.closeHero = this.closeHero.bind(this);
+    this.returnHome = this.returnHome.bind(this);
+
+    this.state = {
+      index: null
+    };
+  }
+
+  componentDidMount() {
+    const { weatherZip, getWeatherByZipCode, match } = this.props;
+    if (!weatherZip) {
+      getWeatherByZipCode(match.params.zip);
+    }
+  }
+
+  closeAndRemoveZip() {
+    const { removeZip, match, history } = this.props;
+    removeZip(match.params.zip);
+    return history.push('/');
+  }
+
+  openSpecificDay(index) {
+    this.setState({ index });
+  }
+
+  closeHero() {
+    this.setState({ index:null });
+  }
+
+  returnHome() {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     const { weatherZip } = this.props;
+    const { index } = this.state;
+    const propsForHero = index !== null ? weatherZip.forecast.forecastday[index] : null;
     return (
       <div className="fiveday">
-        {weatherZip && <h3 className="fiveday__header">5 Day Forcast: {weatherZip.zip}</h3>}
-        {weatherZip && weatherZip.forecast && weatherZip.forecast.forecastday.map(day => <FiveDayCard { ...day } />) }
+        <div className="fiveday__header mb2">
+          {weatherZip && <h3 className="fiveday__header--text">5 Day Forcast: {weatherZip.zip}</h3>}
+          {weatherZip && <h5 className="fiveday__header--updated">{moment(weatherZip.fetchedAt).calendar()}</h5>}
+        </div>
+        <div className="fiveday__days">
+          {weatherZip && weatherZip.forecast && weatherZip.forecast.forecastday.map((day, index) => <FiveDayCard { ...day } key={ `fivedaycard-${day.date}` } showMore={ this.openSpecificDay } index={ index } />) }
+        </div>
+        { propsForHero && <div className="fiveday__hero">
+          <HeroWeatherDisplay { ...propsForHero } close={ this.closeHero } />
+        </div>}
+        <div className="fiveday__links my2" onClick={ this.returnHome }>Back to main screen</div>
+        <div className="fiveday__links" onClick={ this.closeAndRemoveZip }>Delete weather bookmark and return home</div>
       </div>
     );
   }
@@ -21,4 +76,17 @@ const mapStateToProps = (state, ownProps) => {
   return { weatherZip };
 };
 
-export default withRouter(connect(mapStateToProps)(FiveDay));
+const mapDispatchToProps = {
+  getWeatherByZipCode,
+  removeZip
+};
+
+FiveDay.propTypes = {
+  weatherZip: object,
+  getWeatherByZipCode: func,
+  match: object,
+  removeZip: func,
+  history: object
+};
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(FiveDay));
